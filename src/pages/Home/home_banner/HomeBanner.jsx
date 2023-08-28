484f34a35bedc958937a1335df44e14a91b54e08
Ashraf_uddin <dev.fahim2021@gmail.com>
2023-08-29 02:23:55 +0600

    ```Fixed``` Home Banner Bg Image Position

--- src/pages/Home/home_banner/HomeBanner.jsx
+++ src/pages/Home/home_banner/HomeBanner.jsx
@@ -7,6 +7,11 @@ import bg from "/bg.jpg";
 import useFetch from "../../../hooks/useFetch";
 import Img from "../../../components/ui/Img";
 
+import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+import Spinner from "../../../components/ui/Spinner";
+import { HiPlay } from "react-icons/hi2";
+
 
 export default function HomeBanner() {
   const [randomBg, setRandomBg] = useState("")
@@ -33,28 +38,59 @@ export default function HomeBanner() {
       <section className="home_banner">
         <MainWrapper>
           <div className="home_banner__container">
-            <h1
-              className="text-3xl sm:text-4xl lg:text-5xl 2xl:text-6xl font-bold line-clamp-2"
-              title={randomVideo?.title}
-            >
-              {randomVideo?.title}
-            </h1>
-            
-            <p
-              className="mb-2 text-gray-400 text-sm md:text-base lg:text-lg 2xl:text-xl line-clamp-3"
-              title={randomVideo?.overview}
-            >
-              {randomVideo?.overview}
-            </p>
-            <button className="btn_md primary_btn">Watch Trailor</button>
+            {loading ? (
+              <SkeletonTheme baseColor="#202020" highlightColor="#fff">
+                <div className="w-5/6 md:w-80 lg:w-96 animate-pulse duration-0 delay-0">
+                  <Skeleton height={40} />
+                </div>
+              </SkeletonTheme>
+            ) : (
+              <h1
+                className="text-3xl sm:text-4xl lg:text-5xl 2xl:text-6xl font-bold line-clamp-2"
+                title={randomVideo?.title}
+              >
+                {randomVideo?.title}
+              </h1>
+            )}
+
+            {loading ? (
+              <SkeletonTheme baseColor="#202020" highlightColor="#fff">
+                <div className="flex flex-col gap-2 w-11/12 md:w-[560px] animate-pulse duration-150 delay-150">
+                  <Skeleton count={3} />
+                </div>
+              </SkeletonTheme>
+            ) : (
+              <p
+                className="mb-2 text-gray-400 text-sm md:text-base lg:text-lg 2xl:text-xl line-clamp-3"
+                title={randomVideo?.overview}
+              >
+                {randomVideo?.overview}
+              </p>
+            )}
+
+            {loading ? (
+              <SkeletonTheme baseColor="#202020" highlightColor="#fff">
+                <div className="w-32 md:w-48 animate-pulse duration-0 delay-0">
+                  <Skeleton height={40} />
+                </div>
+              </SkeletonTheme>
+            ) : (
+              <button className="btn_md primary_btn flex flex-row items-center gap-2">
+                  <HiPlay /> Watch Now
+              </button>
+            )}
           </div>
         </MainWrapper>
 
-        {!loading && (
+        {!loading ? (
           <div
-            className="w-full h-full absolute right-0 top-0 -z-30 bg-cover bg-center xl:bg-top bg-no-repeat"
+            className="bgBannerImg"
             style={{ backgroundImage: `url(${randomBg})` }}
           />
+        ) : (
+          <div className="flex items-center justify-center">
+            <Spinner />
+          </div>
         )}
       </section>
     </>
